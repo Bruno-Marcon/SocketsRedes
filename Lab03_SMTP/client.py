dce2fa5fb08659a28ab3bd9f983a02c9eb6f9cb1
Bruno Marcon <55155016+Bruno-Marcon@users.noreply.github.com>
2024-11-11 23:36:28 -0300

    adjustment in return logs

--- Lab03_SMTP/client.py
+++ Lab03_SMTP/client.py
@@ -8,10 +8,8 @@ def criar_conexao_mailserver():
     servidor_email = ("smtp.gmail.com", 465)
     cliente_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     cliente_socket.connect(servidor_email)
-    
     contexto_ssl = ssl.create_default_context()
     cliente_socket = contexto_ssl.wrap_socket(cliente_socket, server_hostname="smtp.gmail.com")
-    
     return cliente_socket
 
 # Função para enviar o comando EHLO
@@ -19,6 +17,11 @@ def enviar_helo(cliente_socket):
     comando_helo = 'EHLO Cliente\r\n'
     cliente_socket.send(comando_helo.encode())
     resposta = cliente_socket.recv(1024).decode()
+    if "220" in resposta:
+        print("Comando EHLO aceito.")
+    else:
+        print("Falha ao enviar EHLO.")
+    print("Resposta do servidor ao EHLO:")
     print(resposta)
 
 # Função para autenticar com o servidor
@@ -29,6 +32,11 @@ def autenticar(cliente_socket, usuario, senha):
     cliente_socket.send(comando_auth.encode())
     
     resposta_auth = cliente_socket.recv(1024).decode()
+    if "250" in resposta_auth:
+        print("Autenticação bem-sucedida.")
+    else:
+        print("Falha na autenticação.")
+    print("Resposta do servidor à autenticação:")
     print(resposta_auth)
 
 # Função para enviar o remetente do e-mail
@@ -36,6 +44,11 @@ def enviar_remetente(cliente_socket, remetente):
     comando_remetente = f"MAIL FROM: <{remetente}>\r\n"
     cliente_socket.send(comando_remetente.encode())
     resposta_remetente = cliente_socket.recv(1024).decode()
+    if "235" in resposta_remetente:
+        print(f"Remetente <{remetente}> aceito.")
+    else:
+        print("Falha ao enviar remetente.")
+    print("Resposta do servidor ao comando MAIL FROM:")
     print(resposta_remetente)
 
 # Função para enviar o destinatário
@@ -43,6 +56,11 @@ def enviar_destinatario(cliente_socket, destinatario):
     comando_destinatario = f"RCPT TO: <{destinatario}>\r\n"
     cliente_socket.send(comando_destinatario.encode())
     resposta_destinatario = cliente_socket.recv(1024).decode()
+    if "250" in resposta_destinatario:
+        print(f"Destinatário <{destinatario}> aceito.")
+    else:
+        print("Falha ao enviar destinatário.")
+    print("Resposta do servidor ao comando RCPT TO:")
     print(resposta_destinatario)
 
 # Função para enviar a mensagem do e-mail
@@ -50,17 +68,34 @@ def enviar_mensagem(cliente_socket, assunto, corpo_mensagem):
     comando_data = "DATA\r\n"
     cliente_socket.send(comando_data.encode())
     resposta_data = cliente_socket.recv(1024).decode()
+    if "250" in resposta_data:
+        print("Servidor pronto para receber a mensagem.")
+    else:
+        print("Falha ao iniciar envio da mensagem.")
+    print("Resposta do servidor ao comando DATA:")
     print(resposta_data)
-
+    
     mensagem_email = f"Subject: {assunto}\r\n\r\n{corpo_mensagem}\r\n"
     cliente_socket.send(mensagem_email.encode())
     cliente_socket.send("\r\n.\r\n".encode())
+    resposta_mensagem = cliente_socket.recv(1024).decode()
+    if "354" in resposta_mensagem:
+        print("Mensagem enviada com sucesso.")
+    else:
+        print("Falha ao enviar a mensagem.")
+    print("Resposta do servidor ao envio da mensagem:")
+    print(resposta_mensagem)
 
 # Função para encerrar a conexão
 def encerrar_conexao(cliente_socket):
     comando_quit = "QUIT\r\n"
     cliente_socket.send(comando_quit.encode())
     resposta_quit = cliente_socket.recv(1024).decode()
+    if "250" in resposta_quit:
+        print("Conexão encerrada com sucesso.")
+    else:
+        print("Falha ao encerrar a conexão.")
+    print("Resposta do servidor ao comando QUIT:")
     print(resposta_quit)
     cliente_socket.close()
 
